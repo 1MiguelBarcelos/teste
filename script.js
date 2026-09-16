e51ca66cb7bf53e2a408cf15fd4e9c0f6ff7d556
1MiguelBarcelos <miguelbarceloscosta@gmail.com>
2026-09-16 20:42:51 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// // alert('Seja Bem-vindo!')
+
+
+// // console.log('Nome do usuário inserido:' ,nomeUsuario);
+
+// // var titulo = document.getElementById('msg');
+
+// // titulo.textContent = `Olá, ${nomeUsuario}!`
+
+// var num = parseInt(prompt('Digite um número:'));
+// var num2 = parseInt(prompt('Digite mais um número:'));
+// var num3 = parseInt(num + num2);
+// alert(`${num3}`);
+
+
+var nomeUsuario = prompt('Digite seu nome:');
